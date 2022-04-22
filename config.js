cbf5100bf692b0ddacda9de0e549263b892466b3
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-04-22 22:18:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+let fa = require('fs')
+let chalk = require('chalk')
+
+global.owner = ['6282245409072'] // Put your number here
+global.mods = ['6288226632680'] // Want some help?
+global.prems = ['6285786433832'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'RobbyCatur',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://api.lolhuman.xyz': 'c6670fc7e461b7623a8fdf9f'
+}
+
+// Sticker WM
+global.packname = 'Pudge Invoker'
+global.author = 'Robby Catur'
+
+
+global.multiplier = 69 // The higher, The harder levelup
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
