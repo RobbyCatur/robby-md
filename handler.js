f0eb3a994251947c75c54e0324b814a9ac1113ae
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-04-23 11:36:18 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -121,10 +121,10 @@ module.exports = {
             } catch (e) {
                 console.error(e)
             }
-               if (setting.nyimak) return
+               if (settings.nyimak) return
      	       if (!m.fromMe && setting.self) return
-      	       if (setting.pconly && m.chat.endsWith('g.us')) return
-     	       if (setting.gconly && !m.chat.endsWith('g.us')) return
+      	       if (settings.pconly && m.chat.endsWith('g.us')) return
+     	       if (settings.gconly && !m.chat.endsWith('g.us')) return
     	       if (typeof m.text !== 'string') m.text = ''
                for (let name in global.plugins) {
                 let plugin = global.plugins[name]
